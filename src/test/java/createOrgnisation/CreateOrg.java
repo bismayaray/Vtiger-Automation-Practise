9f6293394fc68f0ac0d462b1a4558ac17aa5f1c7
Bismaya <bismayar19@gmail.com>
2022-12-17 17:08:49 +0530

    TC_100 -> commit

---
+++ src/test/java/createOrgnisation/CreateOrg.java
@@ -0,0 +1,13 @@
+package createOrgnisation;
+
+import org.testng.annotations.Test;
+
+public class CreateOrg 
+{
+	@Test
+	public void TC_100createOrgWithMandatoryFields()
+	{
+		System.out.println("TC_100");
+	}
+
+}
